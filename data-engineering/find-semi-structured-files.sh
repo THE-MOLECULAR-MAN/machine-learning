064bcbf748f2c9d0603fee8aadbef3ce4ca798d8
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-10-10 10:47:40 -0400

    merged with another version in another repo

--- data-engineering/find-semi-structured-files.sh
+++ data-engineering/find-semi-structured-files.sh
@@ -4,7 +4,10 @@
 # Finding semi-structured files on Synology
 
 find /volume1 -type f \( -iname "*.csv" -o -iname "*.xml" -o -iname "*.json" \
-    -o -iname "*.xml" -o -iname "*.eml" \) \
+    -o -iname "*.eml"  -o -iname '*.har'  -o -iname '*.chlsj' \) \
     ! -name 'package.json' ! -path '*@*' ! -ipath '*plex*' \
     ! -ipath '*node*' ! -ipath '*httrack*' ! -ipath '*web_archiving*' \
+    ! -path '*homelab-public*' \
     2>/dev/null
+
+# -o -iname '*.xlsx'  -o -iname '*.xls' -o -iname '*.zip' 
